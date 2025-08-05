58475c74b253f88fdffe4eb6a40a2be44df6703d
adilraees786 <adilraees911@gmail.com>
2025-08-05 16:58:40 +0500

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,28 @@
 
+// import ReactDOM from "react-dom/client";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// import DashboardLayout from "./DashboardLayout/DashboardLayout";
+// import { routelist } from "./Routes/Route";
+
+// export default function App() {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+     
+//           <Route element={<DashboardLayout />} >
+//                 {routelist.map(({ path, element }) => (
+//                   <Route key={path} path={path} element={element} />
+//                 ))}
+//               </Route>
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
+
+
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import DashboardLayout from "./DashboardLayout/DashboardLayout";
 import { routelist } from "./Routes/Route";
@@ -9,15 +31,16 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-     
-          <Route element={<DashboardLayout />} >
-                {routelist.map(({ path, element }) => (
-                  <Route key={path} path={path} element={element} />
-                ))}
-              </Route>
+        <Route element={<DashboardLayout />}>
+         
+          <Route index element={<Navigate to="/dashboard" />} />
+
+       
+          {routelist.map(({ path, element }) => (
+            <Route key={path} path={path} element={element} />
+          ))}
+        </Route>
       </Routes>
     </BrowserRouter>
   );
 }
-
-
